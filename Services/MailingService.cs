a4dd04b9bb90174988e969161504bc96204c3d80
catneep <40044744+catneep@users.noreply.github.com>
2021-06-02 01:29:12 -0500

    Update MailingService.cs

--- Services/MailingService.cs
+++ Services/MailingService.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Threading.Tasks;
+using fan_07.Models;
+
 namespace fan_07.Services
 {
     public interface IMailingService
     {
         //TODO
-        
+        Task<int> GetStatus(Pedido pedido);
+        Task<Distribuidor> AssignDistributor();
         
     }
 }
