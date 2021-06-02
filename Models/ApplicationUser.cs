200d42a016b8c5e7dc64a044f6e95c99da8dec7c
catneep <40044744+catneep@users.noreply.github.com>
2021-06-01 23:47:28 -0500

    Update ApplicationUser.cs

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,6 +4,7 @@ namespace fan_07.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
     }
 }
