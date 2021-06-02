e76d5b30a8a4252665e5a4c85263e531dbcb3c23
catneep <40044744+catneep@users.noreply.github.com>
2021-06-02 01:30:17 -0500

    Update ImagenProducto.cs
    Changed byte[] to string in order to store base64 strings for the images, not optimal but good enuff' for this implementation.

--- Models/ImagenProducto.cs
+++ Models/ImagenProducto.cs
@@ -6,6 +6,6 @@ namespace fan_07.Models
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public Producto Producto { get; set; }
-        public byte[] Imagen { get; set; }
+        public string Imagen { get; set; }
     }
 }
