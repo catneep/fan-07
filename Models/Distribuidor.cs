72bd1f9be2323a9a8bbcfdcba2ae819f0d3a1a0c
Arturo Ochoa <mendozaarturo1221@gmail.com>
2021-06-17 21:45:15 -0500

    lo de Paqueteria
    formPaqueteria, modelo Distribuidor y vista

--- Models/Distribuidor.cs
+++ Models/Distribuidor.cs
@@ -5,7 +5,10 @@ namespace fan_07.Models
     public class Distribuidor
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string Nombre { get; set; }
+        public string NombrePaqueteria { get; set; }
+        public string RFC { get; set; }
+        public string URL { get; set; }
+        public string NombreResponsable { get; set; }
         public string Correo { get; set; }
         public string Telefono { get; set; }
         public decimal Costo { get; set; }
